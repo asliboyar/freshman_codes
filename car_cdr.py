f7a0f20b26460d0b6f82e3a32d4188663b298f62
aboyar <54912300+aboyar@users.noreply.github.com>
2019-10-29 21:34:17 -0500

    Update car_cdr.py

--- car_cdr.py
+++ car_cdr.py
@@ -3,13 +3,11 @@ def cons(a,b):
   def pair(f):
     return f(a,b)
   return pair
- 
- 
+  
 def car(pair):
-
   return (pair(lambda a, b: a))
+
 def cdr(pair):
- 
   return (pair(lambda a, b: b))
 
 print(car(cons(5,4)))
